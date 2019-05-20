5670936d0e5ab79f043c17266fecc0312b66eb2e
Victor Figueroa <ijmrtll@gmail.com>
2019-05-19 17:47:49 -0700

    Update the date.
    Updated the description and the date.

--- coming_soon/js/main.js
+++ coming_soon/js/main.js
@@ -89,7 +89,7 @@
     * ------------------------------------------------------ */
     var ssFinalCountdown = function() {
 
-        var finalDate = '2019/04/01';
+        var finalDate = '2019/09/05';
 
         $('.home-content__clock').countdown(finalDate)
         .on('update.countdown finish.countdown', function(event) {
